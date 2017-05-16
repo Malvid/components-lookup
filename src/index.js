89d269a1a85642bf5d48923689b5fb9d4c83b37b
Tobias Reich <tobias.reich.ich@gmail.com>
2017-05-16 17:36:51 +0200

    Tests and better lookup

--- src/index.js
+++ src/index.js
@@ -20,43 +20,55 @@ const getFiles = function(pattern, cwd) {
 }
 
 /**
+ * Locate and load a file.
+ * @public
+ * @param {Function} fn - Function that returns an array of paths to tell the potential location of a file.
+ * @param {String} cwd - The directory in which to search.
+ * @returns {String} file - Contents of a file.
+ */
+const getFile = function(fn, cwd) {
+
+	// Get an array of paths to tell the location of potential files
+	const locations = fn(id)
+
+	// Look for the data in the same directory as filePath
+	const relativePath = locatePath.sync(locations, { cwd })
+
+	// Only continue when with files
+	if (relativePath==null) return
+
+	// Path to data must be absolute to read it
+	const absolutePath = path.resolve(cwd, relativePath)
+
+	return fs.readFileSync(absolutePath, 'utf8')
+
+}
+
+/**
  * Gather information about a component.
  * @public
  * @param {String} filePath - Absolute path to component.
+ * @param {Object} files - Functions that return an array of paths to tell the potential location of files.
  * @param {String} cwd
- * @param {Function} dataPaths - Function that returns an array of paths to tell the location of potential data files.
  * @returns {Object} component - Information of a component.
  */
-const parseComponent = function(filePath, cwd, dataPaths) {
+const parseComponent = function(filePath, files, cwd) {
 
 	const fileDir = path.dirname(filePath)
 
 	const id   = path.parse(filePath).name
-	const dir  = path.relative(cwd, fileDir)
 	const src  = path.relative(cwd, filePath)
-	const view = fs.readFileSync(filePath, 'utf8')
+	const data = {}
 
-	const data = (() => {
+	Object.keys(files).forEach((key) => {
 
-		// Look for the data in the same directory as filePath
-		const relativeDataPath = locatePath.sync(dataPaths(id), {
-			cwd: fileDir
-		})
+		data[key] = getFile(files[key], fileDir)
 
-		if (relativeDataPath==null) return
-
-		// Path to data must be absolute to read it
-		const absoluteDataPath = path.resolve(fileDir, relativeDataPath)
-
-		return fs.readFileSync(absoluteDataPath, 'utf8')
-
-	})()
+	})
 
 	return {
 		id,
-		dir,
 		src,
-		view,
 		data
 	}
 
@@ -66,13 +78,18 @@ const parseComponent = function(filePath, cwd, dataPaths) {
  * Look for components and gather information about them.
  * @public
  * @param {String} pattern - Files to look for using a glob pattern.
- * @param {Object} opts - Options.
+ * @param {Object} files - Functions that return an array of paths to tell the potential location of files.
+ * @param {?Object} opts - Options.
  * @returns {Array} components - Components and their information.
  */
-module.exports = function(pattern, opts) {
+module.exports = function(pattern, files, opts) {
 
 	if (typeof pattern!=='string') {
 		throw new Error(`'pattern' must be a string`)
+	}
+
+	if (isPlainObj(files)===false) {
+		throw new Error(`'files' must be an object`)
 	}
 
 	if (isPlainObj(opts)===false && opts!=null) {
@@ -80,13 +97,13 @@ module.exports = function(pattern, opts) {
 	}
 
 	opts = Object.assign({
-		cwd       : process.cwd(),
-		dataPaths : () => {}
+		cwd: process.cwd()
 	}, opts)
 
 	// Support relative and absolute paths
 	opts.cwd = path.resolve(opts.cwd)
 
+	// Get all files matching the pattern
 	const filePaths = getFiles(pattern, opts.cwd)
 
 	return filePaths.map((filePath) => {
@@ -94,7 +111,7 @@ module.exports = function(pattern, opts) {
 		// Ensure that path is absolute
 		filePath = path.resolve(opts.cwd, filePath)
 
-		return parseComponent(filePath, opts.cwd, opts.dataPaths)
+		return parseComponent(filePath, files, opts.cwd)
 
 	})
 
--- test/index.js
+++ test/index.js
@@ -5,7 +5,27 @@ const index  = require('./../src/index')
 
 describe('index()', function() {
 
-	it('', function() {
+	it('should throw an error when called without pattern', function() {
+
+		assert.throws(() => index(), `'pattern' must be a string`)
+
+	})
+
+	it('should throw an error when called without files', function() {
+
+		assert.throws(() => index(''), `'files' must be an object`)
+
+	})
+
+	it('should throw an error when called with invalid options', function() {
+
+		assert.throws(() => index('', {}, ''), `'opts' must be an object, null or undefined`)
+
+	})
+
+	it('should return an array', function() {
+
+		assert.isArray(index('*', {}))
 
 	})
 
